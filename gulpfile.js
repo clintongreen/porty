ee8448bbb7bd581d353551eba7e4fece7649823f
Clinton Green - Github <clintrgreen@gmail.com>
2018-05-28 21:27:17 +1200

    Added files

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var bs = require('browser-sync').create();
+
+gulp.task('default', function() {
+    // Code for the default task
+});
+
+gulp.task('browser-sync', ['sass'], function() {
+    bs.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('sass', function () {
+    return gulp.src('scss/*.scss')
+                .pipe(sass())
+                .pipe(gulp.dest('css'))
+                .pipe(bs.reload({stream: true}));
+});
+
+gulp.task('watch', ['browser-sync'], function () {
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', bs.reload);
+});
